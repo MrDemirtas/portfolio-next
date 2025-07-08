8c0e12fb39d0e8bd4f39e0a9bdd46136d56d2a1c
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-07-08 16:35:17 +0300

    getData fonksiyonunda API URL'si, gelen istek başlıklarından alınan protokol ve host bilgileri ile güncellendi; çevresel değişken kullanımı kaldırıldı.

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -1,5 +1,9 @@
+import { headers } from "next/headers";
+
 export const getData = async (endpoint: string) => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-  const url = `${baseUrl}/${endpoint}`;
+  const headersList = await headers();
+  const protocol = headersList.get("x-forwarded-proto") || "http";
+  const host = headersList.get("x-forwarded-host") || "localhost:3000";
+  const url = `${protocol}://${host}/${endpoint}`;
   return await fetch(url).then((res) => res.json());
 };
