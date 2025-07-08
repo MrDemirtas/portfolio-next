89892013a90cff4cf973fccf4ea7b4e88951990f
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-07-08 16:18:08 +0300

    getData fonksiyonundaki API URL'si  güncellendi; NEXT_PUBLIC_API_URL değişkeni NEXT_PUBLIC_BASE_URL olarak değiştirildi.

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -1,5 +1,5 @@
 export const getData = async (endpoint: string) => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
   const url = `${baseUrl}/${endpoint}`;
   return await fetch(url).then((res) => res.json());
 };
